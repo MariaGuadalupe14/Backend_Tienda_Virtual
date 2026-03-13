500e4f91220880af93ce6a81ac25458625b05e58
Maria Guadalupe Clemente Tellez <240613@utxicotepec.edu.mx>
2026-03-13 14:35:27 -0600

    Backend_Tienda_Virtual
    agregué la configuración base en package.json y generé el package-lock.json, creé el archivo principal app.js y añadí reglas de ignorado en .gitignore

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const http = require('http');
+const { parse } = require('path');
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => res.status(200).send({
+    message: 'Bienvenido a la API de la tienda virtual'
+}));
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+module.exports = app;
+
